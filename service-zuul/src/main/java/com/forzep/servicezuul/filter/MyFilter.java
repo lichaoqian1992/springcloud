d08f1c7b166ef60b05ee66b767674c2442097753
lichaoqian <1015598423@qq.com>
2017-12-11 14:40:58 +0800

    change

--- service-zuul/src/main/java/com/forzep/servicezuul/filter/MyFilter.java
+++ service-zuul/src/main/java/com/forzep/servicezuul/filter/MyFilter.java
@@ -47,9 +47,9 @@ public class MyFilter extends ZuulFilter {
 
 
 //    filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
-//    pre：路由之前1
+//    pre：路由之前
 //    routing：路由之时
-//    post： 路由之后111
+//    post： 路由之后
 //    error：发送错误调用
 //    filterOrder：过滤的顺序
 //    shouldFilter：这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
