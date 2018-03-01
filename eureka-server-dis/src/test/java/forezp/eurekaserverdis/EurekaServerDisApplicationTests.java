9a4c71fb887b7bc5f4078e83ff3aaa4ffaf3561d
lichaoqian <1015598423@qq.com>
2018-03-01 11:11:14 +0800

    commit dis

---
+++ eureka-server-dis/src/test/java/forezp/eurekaserverdis/EurekaServerDisApplicationTests.java
@@ -0,0 +1,16 @@
+package forezp.eurekaserverdis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerDisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
